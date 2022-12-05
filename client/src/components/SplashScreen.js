3af4ed7387f667b8ce16bf028db244271c33fabb
EheTeNanDaYoPaimon <98989799+EheTeNanDaYoPaimon@users.noreply.github.com>
2022-12-05 10:21:42 -0500

    edits

---
+++ client/src/components/SplashScreen.js
@@ -0,0 +1,15 @@
+export default function SplashScreen() {
+    return (
+        <div id="splash-screen">
+            <div id="welcome-msg" style = {{"fontSize" : "200px"}} >
+                Welcome! Share the music with others!
+            </div>
+            <div id="about-msg" style = {{"fontSize" : "70px"}} >
+                Want to find a song? You can create and play playlist full of Youtube music. You can play and comment on other people's playlist or make on yourself and share with everyone.
+            </div>
+            <div id="credit-msg" style = {{"fontSize" : "30px"}} >
+                Credit : Huifu Li and others
+            </div>
+        </div>
+    )
+}
---
+++ client/src/components/WorkspaceScreen.js
@@ -0,0 +1,93 @@
+import { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import SongCard from './SongCard.js'
+import MUIEditSongModal from './MUIEditSongModal'
+import MUIRemoveSongModal from './MUIRemoveSongModal'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import { GlobalStoreContext } from '../store/index.js'
+import SearchBar from './SearchBar'
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+/*
+    This React component lets us edit a loaded list, which only
+    happens when we are on the proper route.
+    
+    @author McKilla Gorilla
+*/
+function WorkspaceScreen() {
+    const { store } = useContext(GlobalStoreContext);
+    store.history = useHistory();
+    
+    let modalJSX = "";
+    if (store.isEditSongModalOpen()) {
+        modalJSX = <MUIEditSongModal />;
+    }
+    else if (store.isRemoveSongModalOpen()) {
+        modalJSX = <MUIRemoveSongModal />;
+    }
+
+    console.log("Workspace")
+
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(3),
+          width: 'auto',
+        },
+      }));
+      
+      const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }));
+
+    return (
+         <div>
+        {SearchBar}
+            
+            { "the rest" }
+        
+         </div>
+         
+    )
+}
+
+export default WorkspaceScreen;
